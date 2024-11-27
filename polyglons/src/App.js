8351f4b2eaf06a5e4fc0565fd777fbcf227edbed
harukam122 <mashar0122.yhm@gmail.com>
2024-11-26 20:06:40 -0500

    formatting

--- polyglons/src/App.js
+++ polyglons/src/App.js
@@ -1,17 +1,17 @@
-import "./css/App.css";
-import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { FlyControls, PointerLockControls } from "@react-three/drei";
-import * as THREE from "three";
-import { useEffect, useState } from "react";
-import { useLoader } from "@react-three/fiber";
-import { EffectComposer, Bloom } from "@react-three/postprocessing";
-import Terrain from "./components/perlinTerrain";
-import { generateObjects } from "./components/objectGen";
-import Skybox from "./components/skybox";
-import { clone } from "three/examples/jsm/utils/SkeletonUtils.js";
+import './css/App.css'
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { FlyControls, PointerLockControls } from '@react-three/drei'
+import * as THREE from 'three'
+import { useEffect, useState } from 'react'
+import { useLoader } from '@react-three/fiber'
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import Terrain from './components/perlinTerrain'
+import { generateObjects } from './components/objectGen'
+import Skybox from './components/skybox'
+import { clone } from 'three/examples/jsm/utils/SkeletonUtils.js'
 
-import * as dat from "dat.gui";
+import * as dat from 'dat.gui'
 
 // https://sbcode.net/react-three-fiber/gltfloader/
 // https://threejs.org/docs/#api/en/objects/Mesh
@@ -25,97 +25,105 @@ import * as dat from "dat.gui";
 // music https://www.youtube.com/watch?v=T43D0M8kHFw
 // playlist https://www.youtube.com/watch?v=oKJ2EZnnZRE&list=PL93EE6DF71E5913A7
 function App() {
-  const [params, setParams] = useState({
-    scale: 10,
-    octaves: 8,
-    lacunarity: 2.0,
-    persistence: 1.0,
-    exponentiation: 3.0,
-    height: 5.5,
-  });
-  const scene = new THREE.Scene();
+    const [params, setParams] = useState({
+        scale: 10,
+        octaves: 8,
+        lacunarity: 2.0,
+        persistence: 1.0,
+        exponentiation: 3.0,
+        height: 5.5,
+    })
+    const scene = new THREE.Scene()
 
-  useEffect(() => {
-    const gui = new dat.GUI();
-    gui
-      .add(params, "scale", 0, 10)
-      .name("Scale")
-      .onChange((value) => setParams((prev) => ({ ...prev, scale: value })));
-    gui
-      .add(params, "octaves", 1, 8, 1)
-      .name("Octaves")
-      .onChange((value) => setParams((prev) => ({ ...prev, octaves: value })));
-    gui
-      .add(params, "lacunarity", 1, 3)
-      .step(0.1)
-      .name("Lacunarity")
-      .onChange((value) =>
-        setParams((prev) => ({ ...prev, lacunarity: value }))
-      );
-    gui
-      .add(params, "persistence", 0, 1)
-      .step(0.01)
-      .name("Persistence")
-      .onChange((value) =>
-        setParams((prev) => ({ ...prev, persistence: value }))
-      );
-    gui
-      .add(params, "exponentiation", 0.1, 5)
-      .step(0.1)
-      .name("Exponentiation")
-      .onChange((value) =>
-        setParams((prev) => ({ ...prev, exponentiation: value }))
-      );
-    gui
-      .add(params, "height", 0, 100)
-      .name("Height")
-      .onChange((value) => setParams((prev) => ({ ...prev, height: value })));
+    useEffect(() => {
+        const gui = new dat.GUI()
+        gui.add(params, 'scale', 0, 10)
+            .name('Scale')
+            .onChange((value) =>
+                setParams((prev) => ({ ...prev, scale: value }))
+            )
+        gui.add(params, 'octaves', 1, 8, 1)
+            .name('Octaves')
+            .onChange((value) =>
+                setParams((prev) => ({ ...prev, octaves: value }))
+            )
+        gui.add(params, 'lacunarity', 1, 3)
+            .step(0.1)
+            .name('Lacunarity')
+            .onChange((value) =>
+                setParams((prev) => ({ ...prev, lacunarity: value }))
+            )
+        gui.add(params, 'persistence', 0, 1)
+            .step(0.01)
+            .name('Persistence')
+            .onChange((value) =>
+                setParams((prev) => ({ ...prev, persistence: value }))
+            )
+        gui.add(params, 'exponentiation', 0.1, 5)
+            .step(0.1)
+            .name('Exponentiation')
+            .onChange((value) =>
+                setParams((prev) => ({ ...prev, exponentiation: value }))
+            )
+        gui.add(params, 'height', 0, 100)
+            .name('Height')
+            .onChange((value) =>
+                setParams((prev) => ({ ...prev, height: value }))
+            )
 
-    return () => gui.destroy();
-  }, [params]);
+        return () => gui.destroy()
+    }, [params])
 
-  // https://threejs.org/docs/#api/en/core/BufferGeometry
-  const vertices = new Float32Array([
-    -1.0,
-    -1.0,
-    1.0, // v0
-    1.0,
-    -1.0,
-    1.0, // v1
-    1.0,
-    1.0,
-    1.0, // v2
-    -1.0,
-    1.0,
-    1.0, // v3
-  ]);
+    // https://threejs.org/docs/#api/en/core/BufferGeometry
+    const vertices = new Float32Array([
+        -1.0,
+        -1.0,
+        1.0, // v0
+        1.0,
+        -1.0,
+        1.0, // v1
+        1.0,
+        1.0,
+        1.0, // v2
+        -1.0,
+        1.0,
+        1.0, // v3
+    ])
 
-  const indices = [0, 1, 2, 2, 3, 0];
+    const indices = [0, 1, 2, 2, 3, 0]
 
-  const geometry = new THREE.BufferGeometry();
-  geometry.setIndex(indices);
-  geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+    const geometry = new THREE.BufferGeometry()
+    geometry.setIndex(indices)
+    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3))
 
-  generateObjects(scene);
-  // https://sbcode.net/react-three-fiber/use-loader/
+    generateObjects(scene)
+    // https://sbcode.net/react-three-fiber/use-loader/
 
-  return (
-    <Canvas>
-      <ambientLight intensity={0} />
-      <pointLight position={[0, 15, 0]} intensity="300" />
-      <Terrain params={params} />
-      <Skybox />
+    return (
+        <Canvas>
+            <ambientLight intensity={0} />
+            <pointLight position={[0, 15, 0]} intensity="300" />
+            <Terrain params={params} />
+            <Skybox />
 
-      {/* <mesh geometry={geometry} material={material} /> */}
+            {/* <mesh geometry={geometry} material={material} /> */}
 
-      <primitive object={scene} position={[0, 1, 0]} children-0-castShadow />
-      <EffectComposer>
-        <Bloom intensity={0} luminanceThreshold={0} luminanceSmoothing={0.9} />
-      </EffectComposer>
-      <PointerLockControls dragToLook={true} lookSpeed={0.05} />
-      <FlyControls autoForward={false} movementSpeed={2} />
-    </Canvas>
-  );
+            <primitive
+                object={scene}
+                position={[0, 1, 0]}
+                children-0-castShadow
+            />
+            <EffectComposer>
+                <Bloom
+                    intensity={0}
+                    luminanceThreshold={0}
+                    luminanceSmoothing={0.9}
+                />
+            </EffectComposer>
+            <PointerLockControls dragToLook={true} lookSpeed={0.05} />
+            <FlyControls autoForward={false} movementSpeed={2} />
+        </Canvas>
+    )
 }
 
-export default App;
+export default App
--- polyglons/src/components/perlinTerrain.js
+++ polyglons/src/components/perlinTerrain.js
@@ -1,107 +1,105 @@
-import * as THREE from "three";
-import { createNoise2D } from "simplex-noise";
-import { useRef, useEffect } from "react";
+import * as THREE from 'three'
+import { createNoise2D } from 'simplex-noise'
+import { useRef, useEffect } from 'react'
 
 function createTerrain(params) {
-  var geometry = new THREE.PlaneGeometry(100, 100, 100, 100);
-  var nonIndexedGeometry = geometry.toNonIndexed();
+    var geometry = new THREE.PlaneGeometry(100, 100, 100, 100)
+    var nonIndexedGeometry = geometry.toNonIndexed()
 
-  const material = new THREE.MeshLambertMaterial({
-    vertexColors: true,
-    wireframe: false,
-    flatShading: true,
-  });
-  const terrain = new THREE.Mesh(nonIndexedGeometry, material);
-  terrain.rotation.x = -Math.PI / 2;
+    const material = new THREE.MeshLambertMaterial({
+        vertexColors: true,
+        wireframe: false,
+        flatShading: true,
+    })
+    const terrain = new THREE.Mesh(nonIndexedGeometry, material)
+    terrain.rotation.x = -Math.PI / 2
 
-  const noise2D = createNoise2D();
-  const positions = nonIndexedGeometry.attributes.position.array;
+    const noise2D = createNoise2D()
+    const positions = nonIndexedGeometry.attributes.position.array
 
-  const colors = [];
+    const colors = []
 
-  const G = 2.0 ** (-params.persistence);
-  for (let i = 0; i < positions.length; i += 9) {
-      for (let j = i; j < i + 9; j += 3) {
-          const x = positions[j];
-          const y = positions[j + 1];
+    const G = 2.0 ** -params.persistence
+    for (let i = 0; i < positions.length; i += 9) {
+        for (let j = i; j < i + 9; j += 3) {
+            const x = positions[j]
+            const y = positions[j + 1]
 
-          const radius = 20; // make this adjustable later!
-          const distortScale = 0.1;
-          const distortStrength = 3;
-          const distortNoise = noise2D(x * distortScale, y * distortScale);
-          const distortedRadius = radius + distortNoise * distortStrength;
-          const distFromCenter = Math.sqrt(x * x + y * y) / distortedRadius;
+            const radius = 20 // make this adjustable later!
+            const distortScale = 0.1
+            const distortStrength = 3
+            const distortNoise = noise2D(x * distortScale, y * distortScale)
+            const distortedRadius = radius + distortNoise * distortStrength
+            const distFromCenter = Math.sqrt(x * x + y * y) / distortedRadius
 
-          // cubic taper for smoother transition :)
-          let taper = 1 - Math.pow(distFromCenter, 3);
-          taper = Math.max(0, Math.min(1, taper));
+            // cubic taper for smoother transition :)
+            let taper = 1 - Math.pow(distFromCenter, 3)
+            taper = Math.max(0, Math.min(1, taper))
 
-          let amplitude = 1.0;
-          let frequency = 1.0;
-          let normalization = 0;
-          let total = 0;
+            let amplitude = 1.0
+            let frequency = 1.0
+            let normalization = 0
+            let total = 0
 
-          for (let o = 0; o < params.octaves; o++) {
-              const xs = x / params.scale;
-              const ys = y / params.scale;
-              const noiseVal = noise2D(xs * frequency, ys * frequency) * 0.5 + 0.5;
-              total += noiseVal * amplitude;
-              normalization += amplitude;
-              amplitude *= G;
-              frequency *= params.lacunarity;
-          }
-          total /= normalization;
+            for (let o = 0; o < params.octaves; o++) {
+                const xs = x / params.scale
+                const ys = y / params.scale
+                const noiseVal =
+                    noise2D(xs * frequency, ys * frequency) * 0.5 + 0.5
+                total += noiseVal * amplitude
+                normalization += amplitude
+                amplitude *= G
+                frequency *= params.lacunarity
+            }
+            total /= normalization
 
-          const baseHeight = 1;
-          let final = Math.pow(total, params.exponentiation) * params.height;
+            const baseHeight = 1
+            let final = Math.pow(total, params.exponentiation) * params.height
 
-          // apply taper if it's towards the edge
-          if (distFromCenter > 0.75) {
-              final = THREE.MathUtils.lerp(0, final, taper);
-          }
-          // add a base height for the island
-          if (final > 0) {
-              final += baseHeight;
-          }
-          positions[j + 2] = final;
+            // apply taper if it's towards the edge
+            if (distFromCenter > 0.75) {
+                final = THREE.MathUtils.lerp(0, final, taper)
+            }
+            // add a base height for the island
+            if (final > 0) {
+                final += baseHeight
+            }
+            positions[j + 2] = final
 
-          let color;
-          if (positions[i + 2] > 2.4) {
-              color = new THREE.Color(1.0, 1.0, 1.0);
-          } 
-          else if(positions[i + 2] > 1.7){
-              color = new THREE.Color(0.6, 0.6, 0.6);
-          }
-          else {
-              color = new THREE.Color(0.486, 0.988, 0);
-          }
-          colors.push(...color.toArray());
-      }
+            let color
+            if (positions[i + 2] > 2.4) {
+                color = new THREE.Color(1.0, 1.0, 1.0)
+            } else if (positions[i + 2] > 1.7) {
+                color = new THREE.Color(0.6, 0.6, 0.6)
+            } else {
+                color = new THREE.Color(0.486, 0.988, 0)
+            }
+            colors.push(...color.toArray())
+        }
     }
-  }
 
-  nonIndexedGeometry.setAttribute(
-    "color",
-    new THREE.Float32BufferAttribute(colors, 3)
-  );
-  nonIndexedGeometry.attributes.position.needsUpdate = true;
+    nonIndexedGeometry.setAttribute(
+        'color',
+        new THREE.Float32BufferAttribute(colors, 3)
+    )
+    nonIndexedGeometry.attributes.position.needsUpdate = true
 
-  return terrain;
+    return terrain
 }
 
 export default function Terrain({ params }) {
-    const terrainRef = useRef();
-  
+    const terrainRef = useRef()
+
     useEffect(() => {
-        const currentTerrain = terrainRef.current;
-  
-      if (currentTerrain.children.length > 0) {
-        const oldTerrain = currentTerrain.children[0];
-        currentTerrain.remove(oldTerrain);
-      }
-      const newTerrain = createTerrain(params);
-      currentTerrain.add(newTerrain);
-    }, [params]);
-  
-    return <mesh ref={terrainRef} />;
- }
+        const currentTerrain = terrainRef.current
+
+        if (currentTerrain.children.length > 0) {
+            const oldTerrain = currentTerrain.children[0]
+            currentTerrain.remove(oldTerrain)
+        }
+        const newTerrain = createTerrain(params)
+        currentTerrain.add(newTerrain)
+    }, [params])
+
+    return <mesh ref={terrainRef} />
+}
