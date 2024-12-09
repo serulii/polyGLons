3a0478dc3794ad72d4934973cbf9b4e730b0ac39
harukam122 <mashar0122.yhm@gmail.com>
2024-12-09 02:10:09 -0500

    biome-based objects

--- polyglons/src/components/objectGen.js
+++ polyglons/src/components/objectGen.js
@@ -2,18 +2,36 @@ import { loadModel } from '../utils/utils';
 import { DENSITY } from '../utils/constants';
 import seedrandom from 'seedrandom';
 
-const modelData = await loadModel('/models/tree.glb')
+// load models for different biome types
+const models = {
+    FOREST: [
+        await loadModel('/models/tree.glb'),
+    ],
+    DESERT: [
+        await loadModel('/models/cactus1.glb'),
+    ],
+    SNOWY: [
+        await loadModel('/models/i_love_graphics.glb'),
+    ],
+};
 
 export function generateObj(pos, biomeType, seed) {
-    // TODO: semi-randomly choose an object based on biomeType (use seed)
+    // error-check
+    const biomeModels = models[biomeType];
+    if (!biomeModels || biomeModels.length === 0) {
+        console.warn(`No models available for biome type: ${biomeType}`);
+        return null;
+    }
+
     const rng = seedrandom(seed);
     if (rng() > DENSITY) {
         return null;
     }
-    const model = modelData.scene.clone();
+    const randIdx = Math.floor(rng() * biomeModels.length);
+    const model = biomeModels[randIdx].scene.clone();
 
     // randomly scale
-    const scale = rng() * 0.2 + 0.3; // scale between 0.3 and 0.5
+    const scale = rng() * 0.2 + 0.3; // Scale between 0.3 and 0.5
     model.scale.set(scale, scale, scale);
 
     // randomly rotate around y-axis
