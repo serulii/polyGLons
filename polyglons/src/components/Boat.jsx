df90fc45c9ba4b9664ebfa3a1e789455a69e3973
Sherry Zhang <sherryzhang314@gmail.com>
2024-12-10 18:42:06 -0500

    boat animation works :)

--- polyglons/src/components/Boat.jsx
+++ polyglons/src/components/Boat.jsx
@@ -4,6 +4,7 @@ import React, { useRef, useEffect, useState } from 'react';
 import { useGLTF } from '@react-three/drei';
 import { getIsland } from './Terrain.js';
 import { NUM_ISLANDS, SCENE_DIMENSION } from '../utils/constants.js';
+import { AnimationMixer } from 'three';
 
 const BoatControls = ({ ortho, boundingBoxes, modelRef }) => {
     const velocity = useRef({ x: 0, y: 0, z: 0 }); // speed
@@ -12,8 +13,34 @@ const BoatControls = ({ ortho, boundingBoxes, modelRef }) => {
     const activeKeys = useRef({}); // active keys map
     const targetQuaternion = useRef(new THREE.Quaternion()); // use quaternions for shortest path (otherwise boat may rotate the wrong way)
     const [initialized, setInitialized] = useState(false);
-    const { scene } = useGLTF('./models/boat.gltf');
+    const { scene, animations } = useGLTF('./models/boat_2.gltf');
+    const mixerRef = useRef();
     scene.scale.set(0.2, 0.2, 0.2);
+
+    // boat animation handling
+    useEffect(() => {
+        if (animations && animations.length > 0) {
+            const mixer = new AnimationMixer(scene);
+            mixerRef.current = mixer;
+
+            // play first animation by default
+            const action = mixer.clipAction(animations[0]);
+            action.play();
+
+            // clean up mixer
+            return () => {
+                mixer.stopAllAction();
+                mixer.dispose();
+            };
+        }
+    }, [animations, scene]);
+
+    // update the mixer on each frame
+    useFrame((_, delta) => {
+        if (mixerRef.current) {
+            mixerRef.current.update(delta); // Advance the animation
+        }
+    });
 
     // key press handling
     useEffect(() => {
@@ -44,7 +71,7 @@ const BoatControls = ({ ortho, boundingBoxes, modelRef }) => {
         if (boundingBoxes.length != NUM_ISLANDS * 2) {
             return;
         }
-        
+
         // randomly position boat
         let x, z;
         do {
@@ -67,20 +94,20 @@ const BoatControls = ({ ortho, boundingBoxes, modelRef }) => {
             if (activeKeys.current['w'] || activeKeys.current['ArrowUp']) {
                 vel.x -= acceleration;
                 vel.z += acceleration;
-                euler.set(0, 7 * Math.PI / 4.0, 0);
+                euler.set(0, (7 * Math.PI) / 4.0, 0);
                 newRotation = true;
             }
             if (activeKeys.current['s'] || activeKeys.current['ArrowDown']) {
                 vel.x += acceleration;
                 vel.z -= acceleration;
-                euler.set(0, 3 * Math.PI / 4.0, 0);
+                euler.set(0, (3 * Math.PI) / 4.0, 0);
                 newRotation = true;
                 // modelRef.current.rotation.set(0, 1.6, 0);
             }
             if (activeKeys.current['d'] || activeKeys.current['ArrowRight']) {
                 vel.x -= acceleration;
                 vel.z -= acceleration;
-                euler.set(0, 5 * Math.PI / 4, 0);
+                euler.set(0, (5 * Math.PI) / 4, 0);
                 newRotation = true;
                 // targetRotation.current = { x: 0, y: Math.PI, z: 0 };
                 // modelRef.current.rotation.set(0, 3.14, 0);
@@ -88,7 +115,7 @@ const BoatControls = ({ ortho, boundingBoxes, modelRef }) => {
             if (activeKeys.current['a'] || activeKeys.current['ArrowLeft']) {
                 vel.x += acceleration;
                 vel.z += acceleration;
-                euler.set(0, 1 * Math.PI / 4, 0);
+                euler.set(0, (1 * Math.PI) / 4, 0);
                 newRotation = true;
                 // targetRotation.current = { x: 0, y: 0, z: 0 };
                 // modelRef.current.rotation.set(0, 0, 0);
@@ -121,8 +148,7 @@ const BoatControls = ({ ortho, boundingBoxes, modelRef }) => {
             }
         }
     });
-
     return initialized ? <primitive ref={modelRef} object={scene} /> : null;
-}
+};
 
 export default BoatControls;
